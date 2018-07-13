4316b4962044bc343658d36394fc2ea0ebeb07c3
Dasha <37613468+dashaHsh@users.noreply.github.com>
2018-07-13 14:09:00 +0300

    Add files via upload

--- news-app-v3/js/firestore.js
+++ news-app-v3/js/firestore.js
@@ -1,12 +1,35 @@
 const FirestoreInit = (function () {
+    let instance;
+
     // Initialize Firebase
     let config = {
         apiKey: "AIzaSyBzQy_hQRNiZ5typgz23-1d1RwZnzAGnp0",
         authDomain: "news-app-ab643.firebaseapp.com",
         databaseURL: "https://news-app-ab643.firebaseio.com",
         projectId: "news-app-ab643",
-        storageBucket: "",
+        storageBucket: "news-app-ab643.appspot.com",
         messagingSenderId: "767379270164"
     };
     firebase.initializeApp(config);
+
+    // Init DataBase
+    let db = firebase.firestore();
+    const settings = {timestampsInSnapshots: true};
+    db.settings(settings);
+    
+    function getDb() {
+        return db;
+    }
+    
+    function createInstance() {
+        return {
+            getDb
+        }
+    }
+
+    return {
+        getInstance() {
+            return instance || (instance = createInstance());
+        }
+    }
 })();
