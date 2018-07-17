758fb5762a04cfd2f5f26990fc30bf2da9fbfd0d
Dasha <37613468+dashaHsh@users.noreply.github.com>
2018-07-17 18:22:30 +0300

    Add files via upload

--- news-app-v3/js/authentication/sign-in.js
+++ news-app-v3/js/authentication/sign-in.js
@@ -1,50 +1,52 @@
 class SignInPage {
     constructor() {
         this.html = `
-        <form name="signin-form">
-            <div class="card-content">
-                <div class="container">
-                
-                    <div class="card-heading">
-                        <span class="brand-logo left">NewsApp</span>
-                        <span class="card-title left">/</span>
-                        <span class="card-title left">Sign In</span>
-                    </div>
+        <div class="card">
+            <form name="signin-form">
+                <div class="card-content">
+                    <div class="container">
                     
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input id="email" type="text" name="email" autocomplete="off">
-                            <label for="email">Email</label>
+                        <div class="card-heading">
+                            <span class="brand-logo left">NewsApp</span>
+                            <span class="card-title left">/</span>
+                            <span class="card-title left">Sign In</span>
                         </div>
                         
-                        <div class="input-field col s12">
-                            <input id="password" type="password" name="password" autocomplete="new-password">
-                            <label for="password">Password</label>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="email" type="text" name="email" autocomplete="off">
+                                <label for="email">Email</label>
+                            </div>
+                            
+                            <div class="input-field col s12">
+                                <input id="password" type="password" name="password" autocomplete="new-password">
+                                <label for="password">Password</label>
+                            </div>
+                            
+                            <a class="btn-flat trouble-link" id="helpBtn">Trouble signing in?</a>
                         </div>
-                        
-                        <a class="btn-flat trouble-link" id="helpBtn">Trouble signing in?</a>
+                    
                     </div>
-                
                 </div>
-            </div>
-            
-            <div class="card-action">
-                <div class="container btns-wrapper">
-                    <button class="btn waves-effect waves-light" type="submit" name="action" id="signInBtnPrimary">
-                        <span>Sign In</span>
-                        <i class="material-icons right">arrow_forward</i>
-                    </button>
-                    
-                    <div>
-                        <span class="action-text">Don't have an account?</span>
-                        <button class="btn-flat waves-effect waves-teal" type="button" data-toggle="signUp" name="toggleBtn" id="signUpBtnSecondary">
-                            <span>Sign Up</span>
-                            <i class="material-icons left">create</i>
+                
+                <div class="card-action">
+                    <div class="container btns-wrapper">
+                        <button class="btn waves-effect waves-light" type="submit" name="action" id="signInBtnPrimary">
+                            <span>Sign In</span>
+                            <i class="material-icons right">arrow_forward</i>
                         </button>
+                        
+                        <div>
+                            <span class="action-text">Don't have an account?</span>
+                            <button class="btn-flat waves-effect waves-teal" type="button" data-toggle="signUp" name="toggleBtn" id="signUpBtnSecondary">
+                                <span>Sign Up</span>
+                                <i class="material-icons left">create</i>
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </form>                                                            
+            </form>    
+        </div>                                                        
         `;
     }
 
